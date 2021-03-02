6e7b6d7cb3761242f3af9979978f91703270970c
kawada68 <nodoguro68@gmail.com>
2021-03-02 21:07:44 +0900

    ヘッダーのHTML構造

---
+++ php/views/templete/header.php
@@ -0,0 +1,15 @@
+<header class="header">
+    <div class="header__inner">
+        <div class="logo header__logo">
+            <h1 class="header__logo"><a href="index.php"></a></h1>
+        </div>
+
+        <nav class="header-nav">
+            <ul class="header-nav__inner">
+                <li class="header-nav__item"><a href="index.php" class="header-nav__link">トップページ</a></li>
+                <li class="header-nav__item"><a href="login.php" class="header-nav__link">ログイン</a></li>
+                <li class="header-nav__item"><a href="signup.php" class="header-nav__link">ユーザー登録</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
