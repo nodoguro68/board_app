b33dc0c493bf8dbf4624ca468b2416d31433c417
kawada68 <nodoguro68@gmail.com>
2021-03-02 21:11:05 +0900

    フッターのHTML構造

---
+++ php/views/templete/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="footer__inner">
+        <div class="logo footer__logo">
+            <!-- <img src="" alt=""> -->
+        </div>
+
+        <nav class="footer-nav">
+            <ul class="footer-nav__inner">
+                <li class="footer-nav__item"><a href="" class="footer-nav__link">利用規約</a></li>
+                <li class="footer-nav__item"><a href="" class="footer-nav__link">プライバシーポリシー</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
