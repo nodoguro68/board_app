7f72a75513841e0f71e7412f4c2100cd3ae0fb4f
kawada68 <nodoguro68@gmail.com>
2021-03-03 18:05:15 +0900

    退会説明画面　HTML

---
+++ php/page/signout_description.php
@@ -0,0 +1,35 @@
+<?php  
+
+$page_title = '退会説明ページ';
+
+require_once 'templete/head.php';
+require_once 'templete/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+        <div class="link__inner">
+            <a href="mypage.php" class="return__link">＞マイページへ戻る</a>
+        </div>
+
+        <section class="section">
+            <div class="section__head">
+                <h2 class="title">退会ページ</h2>
+            </div>
+
+            <div class="section__main">
+                <p>退会処理の説明</p>
+            </div>
+        </section>
+
+        <div class="link__inner">
+            <a href="signout.php" class="return__link">退会する</a>
+        </div>
+    </div>
+</main>
+
+<?php require_once 'templete/footer.php';?>
+
+</body>
+
+</html>
