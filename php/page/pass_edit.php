8c3bfbf29ce239c70583661de1410ec1aea32b8f
kawada68 <nodoguro68@gmail.com>
2021-03-03 15:48:35 +0900

    パスワード変更画面　HTML

---
+++ php/page/pass_edit.php
@@ -0,0 +1,47 @@
+<?php  
+
+$page_title = 'パスワード変更ページ';
+
+require_once 'templete/head.php';
+require_once 'templete/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+
+        <!-- フォーム -->
+        <form method="POST" class="form form__pass-edit">
+            <div class="form__head">
+                <h2 class="title form__title">パスワード変更</h2>
+            </div>
+            <div class="form__main">
+                <div class="form__group">
+                    <label class="form__label">古いパスワード</label>
+                    <input type="password" name="old_pass" value="" class="form__input" id="pass">
+                    <span class="err-msg"></span>
+                </div>
+                <div class="form__group">
+                    <label class="form__label">新しいパスワード<span class="note">*半角英数字8文字以上で入力してください</span></label>
+                    <input type="password" name="new_pass" value="" class="form__input" id="pass">
+                    <span class="err-msg"></span>
+                </div>
+                <div class="form__group">
+                    <label class="form__label">新しいパスワード（再入力）</label>
+                    <input type="password" name="new_pass_re" value="" class="form__input" id="new_pass_re">
+                    <span class="err-msg">></span>
+                </div>
+                <input type="hidden" name="csrf_token" value="">
+                <div class="form__foot">
+                    <input type="submit" value="変更" class="btn-submit">
+                </div>
+            </div>
+        </form>
+
+    </div>
+</main>
+
+<?php require_once 'templete/footer.php';?>
+
+</body>
+
+</html>
