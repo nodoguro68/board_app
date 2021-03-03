11fe7bf7cb30bed8fe89876525e93084dd1863f1
kawada68 <nodoguro68@gmail.com>
2021-03-03 18:59:06 +0900

    プロフィール画面 HTML

---
+++ php/page/profile.php
@@ -0,0 +1,39 @@
+<?php  
+
+
+
+$page_title = 'プロフィール';
+
+require_once 'templete/head.php';
+require_once 'templete/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+
+    <section class="section">
+        <div class="section__head">
+            <h2 class="title">プロフィール</h2>
+        </div>
+        
+        
+        <div class="section__main">
+            <div class="profile__inner">
+                <div class="profile__item-icon">
+                    <img src="プロフィールアイコン画像" alt="">
+                </div>
+                <div class="profile__item">
+                    
+                </div>
+            </div>
+        </div>
+    
+    </section> 
+
+    </div>
+</main>
+
+<?php require_once 'templete/footer.php';?>
+
+</body>
+</html>
